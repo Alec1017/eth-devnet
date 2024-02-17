57974ead4f128eac5250e285e77dbc9602a516e7
Alec DiFederico <alecdifederico@gmail.com>
2024-02-17 16:34:03 -0500

    updated network params

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-kurtosis clean -a && kurtosis run --enclave devnet github.com/kurtosis-tech/ethereum-package "$(cat $(pwd)/network_params.yaml)"
+kurtosis clean -a && kurtosis run --enclave devnet github.com/kurtosis-tech/ethereum-package "$(cat ./network_params.yaml)"
